4148c99fb3ae3e6516e365542ff4d3a060031362
Mert Saygı <mertsaygi@gmail.com>
2023-11-27 03:02:19 +0100

    fix: allowed hosts for csrf

--- pygmyui/pygmyui/settings.py
+++ pygmyui/pygmyui/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import socket
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    CELERY_ALWAYS_EAGER=(bool, True),
+    SENTRY_ENABLED=(bool, False),
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
