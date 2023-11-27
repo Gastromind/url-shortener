98e51c48ecd6639a040b46f079cdb3437bbe04de
Mert Saygı <mertsaygi@gmail.com>
2023-11-27 03:26:06 +0100

    fix: allowed hosts for csrf

--- pygmyui/pygmyui/settings.py
+++ pygmyui/pygmyui/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
+from django.core.management.utils import get_random_secret_key
 
-import os
-import socket
 import environ
+import socket
+import os
 
 env = environ.Env(
     # set casting, default value
